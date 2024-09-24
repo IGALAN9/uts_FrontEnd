22590e8d822b517a3600aaa0aad7cec637a033e1
Galang <Galangafdalahharsa88@gmail.com>
2024-09-24 21:32:33 +0700

    Swiper bisa

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ $(document).ready(function(){
     $('.buttons[data-filter="sport"]').trigger('click');
 
 });
+
+
+$(document).ready(function() {
+    var swiper = new Swiper('.review-slider', {
+        loop: true,
+        grabCursor: true,
+        spaceBetween: 20,
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            640: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 3,
+            },
+        },
+    });
+});
